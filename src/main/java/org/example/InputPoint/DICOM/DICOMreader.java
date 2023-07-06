f40d5130135bab3e06bf2ab0c744ab66cc6a5381
xristina <ckaralka@csd.auth.gr>
2023-07-06 22:02:46 +0300

    dicom read tag names and sequences added

--- src/main/java/org/example/InputPoint/DICOM/DICOMreader.java
+++ src/main/java/org/example/InputPoint/DICOM/DICOMreader.java
@@ -1,13 +1,38 @@
 package org.example.InputPoint.DICOM;
 
-import org.dcm4che3.data.Attributes;
-import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.*;
 import org.dcm4che3.io.DicomInputStream;
+import org.dcm4che3.util.TagUtils;
 
 import java.io.File;
 import java.io.IOException;
 
 public class DICOMreader {
+
+    public static void readAttributes(Attributes attributes, boolean isSQ) {
+        int[] tags = attributes.tags();
+
+        if(isSQ)
+            System.out.println("\t>> Item");
+
+        for (int tag : tags) {
+            String value = attributes.getString(tag);
+            String tagCode = TagUtils.toString(tag);
+            String tagName = ElementDictionary.keywordOf(tag, null);
+
+            VR vr = attributes.getVR(tag);
+
+            System.out.println((isSQ ? "\t " : "") + tagCode + "\t" + tagName + "\t" + vr + "\t" + value);
+
+            if(vr == VR.SQ) {
+                Sequence sq = attributes.getSequence(tag);
+                System.out.println("\t# items = " + sq.size() + " [");
+                for(Attributes sqItem : sq)
+                    readAttributes(sqItem, true);
+                System.out.println("]");
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -15,15 +40,12 @@ public class DICOMreader {
 
         try (DicomInputStream dis = new DicomInputStream(new File(filePath))) {
             Attributes attributes = dis.readDataset();
-
-            System.out.println(attributes);
-
-            String patientWeight = attributes.getString(Tag.PatientWeight);
-            System.out.println("Patient Weight: " + patientWeight);
-
+            readAttributes(attributes, false);
+            // System.out.println(attributes);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
