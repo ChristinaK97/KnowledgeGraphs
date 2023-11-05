8db9e16c3ae6d3c5a02f78d78248a72e208b2eb6
xristina <ckaralka@csd.auth.gr>
2023-11-06 01:51:42 +0200

    docker container can now access gpu

--- AAExpansion/ConnectionPoint.py
+++ AAExpansion/ConnectionPoint.py
@@ -1,3 +1,9 @@
+import torch
+if torch.cuda.is_available():
+    print("Cuda available :",  torch.cuda.get_device_name(torch.cuda.current_device()))
+else:
+    print("Cuda NOT available")
+
 import pandas as pd
 
 from source.InterpretHeaders import InterpretHeaders
@@ -6,9 +12,9 @@ path = "resources\\Metainventory_Version1.0.0.csv"
 inputDataset = "resources\\Data_test_Encrypt_Repaired.csv"
 outputPath = "resources\\abbrevExpansionResults.json"
 
-headers = pd.read_csv(inputDataset, delimiter=";").columns.to_list()
+# headers = pd.read_csv(inputDataset, delimiter=";").columns.to_list()
 
-InterpretHeaders(headers, path, outputPath)
+# InterpretHeaders(headers, path, outputPath)
 # SciSpacyEntityLinker(outputPath)
 
 """
