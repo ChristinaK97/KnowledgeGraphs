04c9e40c837a39bba8040c8f55f18038766c265c
xristina <ckaralka@csd.auth.gr>
2023-11-20 19:30:12 +0200

    Kafka: Create topic + publish with retries

--- KnowledgeGraphsJava/src/main/java/org/example/KnowledgeGraphsJavaApp.java
+++ KnowledgeGraphsJava/src/main/java/org/example/KnowledgeGraphsJavaApp.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.Retryable;
 
 @SpringBootApplication
+@Retryable
 public class KnowledgeGraphsJavaApp {
     public static void main( String[] args ) {
         SpringApplication.run(KnowledgeGraphsJavaApp.class, args);
