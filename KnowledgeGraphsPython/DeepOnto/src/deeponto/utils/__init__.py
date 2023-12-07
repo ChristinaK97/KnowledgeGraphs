c1df5b6ebf022e317f3d594a615ccf00b9fe6bfe
xristina <ckaralka@csd.auth.gr>
2023-12-07 13:02:18 +0200

    Bertmap: fixed duplicate log messages

--- KnowledgeGraphsPython/DeepOnto/src/deeponto/utils/__init__.py
+++ KnowledgeGraphsPython/DeepOnto/src/deeponto/utils/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .logging import create_logger, banner_message
+# from .logging import create_logger, banner_message
 from .data_utils import *
 from .file_utils import *
 from .text_utils import *
